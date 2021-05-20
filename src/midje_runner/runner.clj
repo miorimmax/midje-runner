2b7779181702f77f39feddfb5d4c4bc004b37d65
WON SEOB SEO <pghoou@gmail.com>
2021-05-20 14:45:22 +0300

    feat: Support auto testing and specific namespace testing

--- src/midje_runner/runner.clj
+++ src/midje_runner/runner.clj
@@ -2,4 +2,9 @@
   (:require [midje.repl :as mr]))
 
 (defn -main [& args]
-  (mr/load-facts :all))
+  (let [[arg1 & arg-rest] (map symbol args)]
+    (case arg1
+      'auto (mr/autotest)
+      nil (mr/load-facts :all)
+
+      (apply mr/load-facts (conj arg-rest arg1)))))
